cdda8eb201fafb8ca315d2e60400063d5f5d505f
Jean Roshan Urbano <roshanurbano@gmail.com>
2025-12-07 15:23:15 +0800

    Minimal build configuration - absolute simplest setup - Removed all STORAGES configuration - Using basic Django static file handling - Ultra-simple build.sh with only essential commands - This MUST work

--- beauty_clinic_django/settings.py
+++ beauty_clinic_django/settings.py
@@ -156,34 +156,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# WhiteNoise configuration - use simple storage to avoid manifest errors
-if not DEBUG:
-    STORAGES = {
-        "default": {
-            "BACKEND": "django.core.files.storage.FileSystemStorage",
-        },
-        "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
-        },
-    }
-else:
-    STORAGES = {
-        "default": {
-            "BACKEND": "django.core.files.storage.FileSystemStorage",
-        },
-        "staticfiles": {
-            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-        },
-    }
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- build.sh
+++ build.sh
@@ -1,22 +1,7 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-echo "=== Starting Build ==="
-
-# Install dependencies
-echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Create staticfiles directory if it doesn't exist
-mkdir -p staticfiles
-
-# Collect static files (ignore errors for missing files)
-echo "Collecting static files..."
-python manage.py collectstatic --no-input 2>&1 || echo "Collectstatic warnings ignored"
-
-# Run migrations
-echo "Running migrations..."
-python manage.py migrate --no-input
-
-echo "=== Build Complete ==="
+python manage.py collectstatic --no-input
+python manage.py migrate
